eb1960e8759589902dd5032c1eb8668818eea591
Ton Pham <55113025+TonPham1995@users.noreply.github.com>
2020-03-30 20:18:38 -0700

    Initial Commit

---
+++ MyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by Townsite Grocery on 2020-03-30.
+//  Copyright © 2020 Townsite Grocery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
