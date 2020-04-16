9c44cd48efde89c2f5a97fc35b613e467deda49f
ablacquiere <ablacquiere@users.noreply.github.com>
2020-04-16 08:49:47 -0700

    Added URLSession approach to retrieving JSON data. Parsing data using Codable structures.

--- MyProject/ViewController.swift
+++ MyProject/ViewController.swift
@@ -17,30 +17,40 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url: URL = URL(string: "http://ddragon.leagueoflegends.com/cdn/10.7.1/data/en_US/champion.json")!
-        var request: URLRequest = URLRequest(url: url)
-        request.httpMethod = "GET"
-       
-        NSURLConnection.sendAsynchronousRequest(request, queue: .main) { (response, data, error) in
-            
-            guard let data = data else {
-                print(error)
-                return
-            }
-            
-            let responseString: String! = String(data: data, encoding: .utf8)
-            
-            do {
-                
-                let jsonDecoder: JSONDecoder = JSONDecoder()
-                self.data = try jsonDecoder.decode([ChampionModel].self, from: data)
-                
-                self.collectionView.reloadData()
-                
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
+//        let url: URL = URL(string: "http://ddragon.leagueoflegends.com/cdn/10.7.1/data/en_US/champion.json")!
+//        var request: URLRequest = URLRequest(url: url)
+//        request.httpMethod = "GET"
+//
+//        NSURLConnection.sendAsynchronousRequest(request, queue: .main) { (response, data, error) in
+//
+//            guard let data = data else {
+//                print(error)
+//                return
+//            }
+//
+//            let responseString: String! = String(data: data, encoding: .utf8)
+//
+//            do {
+//
+//                let jsonDecoder: JSONDecoder = JSONDecoder()
+//                self.data = try jsonDecoder.decode([ChampionModel].self, from: data)
+//
+//                self.collectionView.reloadData()
+//
+//            } catch {
+//                print(error.localizedDescription)
+//            }
+//        }
+//    }
+    
+    if let url = URL(string: "https://ddragon.leagueoflegends.com/cdn/10.7.1/data/en_US/champion.json") {
+           URLSession.shared.dataTask(with: url) { data, response, error in
+               if let data = data {
+                   let dragon = try! JSONDecoder().decode(Dragon.self, from: data)
+                    print(dragon)
+               }
+           }.resume()
+       }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -58,3 +68,119 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
 }
 
+struct Dragon: Codable {
+    var type: String
+    var format: String
+    var version: String
+    var data: Champion
+}
+
+struct Champion: Codable {
+    var name: String
+    var title: String
+    var blurb: String
+    var key: String
+    
+    enum CodingKeys: String, CodingKey {
+        case data = "data"
+        case aatrox = "Aatrox"
+        case alistar = "Alistar"
+        case name = "name"
+        case title = "title"
+        case blurb = "blurb"
+        case key = "key"
+    }
+
+
+    // Decoding
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let champion = try container.nestedContainer(keyedBy: CodingKeys.self, forKey:.alistar)
+        name = try champion.decode(String.self, forKey: .name)
+        title = try champion.decode(String.self, forKey: .title)
+        blurb = try champion.decode(String.self, forKey: .blurb)
+        key = try champion.decode(String.self, forKey: .key)
+        
+
+//        let info = try aatrox.nestedContainer(keyedBy: CodingKeys.self, forKey: .info)
+//        attack = try info.decode(String.self, forKey: .attack)
+//        defense = try info.decode(String.self, forKey: .defense)
+//        magic = try info.decode(String.self, forKey: .magic)
+    }
+    
+
+    
+    // Encoding
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(title, forKey: .title)
+        try container.encode(blurb, forKey: .blurb)
+ 
+    }
+}
+
+
+extension JSONDecoder {
+
+    func decode<T: Decodable>(_ type: T.Type, from data: Data, keyedBy key: String?) throws -> T {
+        if let key = key {
+            // Pass the top level key to the decoder.
+            userInfo[.jsonDecoderRootKeyName] = key
+
+            let root = try decode(DecodableRoot<T>.self, from: data)
+            return root.value
+        } else {
+            return try decode(type, from: data)
+        }
+    }
+
+}
+
+extension CodingUserInfoKey {
+
+    static let jsonDecoderRootKeyName = CodingUserInfoKey(rawValue: "rootKeyName")!
+
+}
+
+struct DecodableRoot<T>: Decodable where T: Decodable {
+
+    private struct CodingKeys: CodingKey {
+
+        var stringValue: String
+        var intValue: Int?
+
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+
+        init?(intValue: Int) {
+            self.intValue = intValue
+            stringValue = "\(intValue)"
+        }
+
+        static func key(named name: String) -> CodingKeys? {
+            return CodingKeys(stringValue: name)
+        }
+
+    }
+
+    let value: T
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        guard
+            let keyName = decoder.userInfo[.jsonDecoderRootKeyName] as? String,
+            let key = CodingKeys.key(named: keyName) else {
+                throw DecodingError.valueNotFound(
+                    T.self,
+                    DecodingError.Context(codingPath: [], debugDescription: "Value not found at root level.")
+                )
+        }
+
+        value = try container.decode(T.self, forKey: key)
+    }
+
+}
